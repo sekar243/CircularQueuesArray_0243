3e6c5a063e9a5b30f6e67b12f6124c14b9cc8c43
sekar243 <kinasekin6602@gmail.com>
2025-06-10 10:46:46 +0700

    menghapus atau mengambil elemen

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queues {
     int FRONT, REAR, max = 5;
-    int queues_array[5];
+    int queue_array[5];
 
 public:
     Queues() {
@@ -35,5 +35,10 @@ public:
             else    
                 REAR = REAR +1;
         }
+        queue_array[REAR] = num;
+    }
+
+    void remove() {
+        //
     }
 };
